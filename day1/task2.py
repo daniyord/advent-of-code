769d451d91d60546129fc2866a0f1e0101e6a60d
Daniel Yordanov <daniyord@gmail.com>
2024-12-01 15:53:48 +0200

    -

--- day1/task2.py
+++ day1/task2.py
@@ -1,3 +1,14 @@
+def construct_dict(list):
+    constructed_dict = dict()
+
+    for item in list:
+        if item in constructed_dict:
+            constructed_dict[item] += 1
+        else:
+            constructed_dict[item] = 1
+
+    return constructed_dict
+
 
 left_list = []
 right_list = []
@@ -9,6 +20,7 @@ with open('input.txt', 'r') as file:
         left_list.append(int(line_parts[0]))
         right_list.append(int(line_parts[1]))
 
+# 0(n*n)
 result = 0
 
 for left in left_list:
@@ -19,5 +31,17 @@ for left in left_list:
             count += 1
 
     result += (left * count)
+
+print(result)
+
+# 0(n)
+result = 0
+
+left_dict = construct_dict(left_list)
+right_dict = construct_dict(right_list)
+
+for key in left_dict:
+    if key in right_dict:
+        result += key * left_dict[key] * right_dict[key]
 
 print(result)
