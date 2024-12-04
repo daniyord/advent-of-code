c8f58783e7c7c61f037a9f9412841fd81c1c4cd9
Daniel Yordanov <daniyord@gmail.com>
2024-12-04 08:38:46 +0200

    -

--- day4/task1.py
+++ day4/task1.py
@@ -14,14 +14,14 @@ def get_data():
 
 
 def check_field(matrix, x, y):
-    if y < len(matrix[0]) - 3:
+    if y <= len(matrix[0]) - 3:
         if matrix[x][y] == "X" and matrix[x][y+1] == "M" and matrix[x][y+2] == "A" and matrix[x][y+3] == "S":
             return True
 
         if matrix[x][y] == "S" and matrix[x][y+1] == "A" and matrix[x][y+2] == "M" and matrix[x][y+3] == "X":
             return True
 
-    if x < len(matrix) - 3:
+    if x <= len(matrix) - 3:
         if matrix[x][y] == "X" and matrix[x+1][y] == "M" and matrix[x+2][y] == "A" and matrix[x+3][y] == "S":
             return True
 
