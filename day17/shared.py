9425415ae847054111066fd495dd2892e84eb08b
Daniel Yordanov <daniel.yordanov@a1.bg>
2024-12-19 15:13:23 +0200

    -

--- day17/shared.py
+++ day17/shared.py
@@ -14,3 +14,32 @@ def get_input(filename):
                 code = [int(x.strip()) for x in line.split(":")[1].split(",")]
 
     return (r_a, r_b, r_c, code)
+
+
+def get_literal(code, i_p):
+    return code[i_p + 1]
+
+
+def get_combo(code, i_p, r_a, r_b, r_c):
+    match code[i_p + 1]:
+        case 0:
+            return code[i_p + 1]
+        case 1:
+            return code[i_p + 1]
+        case 2:
+            return code[i_p + 1]
+        case 3:
+            return code[i_p + 1]
+        case 4:
+            return r_a
+        case 5:
+            return r_b
+        case 6:
+            return r_c
+
+
+def op_jnz(i_p, r_a, literal):
+    if r_a == 0:
+        return i_p + 2
+    else:
+        return literal
--- day17/task1.py
+++ day17/task1.py
@@ -1,37 +1,7 @@
-from shared import get_input
+from shared import get_input, get_literal, get_combo, op_jnz
 
 r_a, r_b, r_c, code = get_input("input.txt")
 i_p = 0
-
-
-def get_literal(code, i_p):
-    return code[i_p + 1]
-
-
-def get_combo(code, i_p, r_a, r_b, r_c):
-    match code[i_p + 1]:
-        case 0:
-            return code[i_p + 1]
-        case 1:
-            return code[i_p + 1]
-        case 2:
-            return code[i_p + 1]
-        case 3:
-            return code[i_p + 1]
-        case 4:
-            return r_a
-        case 5:
-            return r_b
-        case 6:
-            return r_c
-
-
-def op_jnz(r_a, literal):
-    if r_a == 0:
-        return i_p + 2
-    else:
-        return literal
-
 
 output = []
 while (i_p < len(code)):
@@ -55,7 +25,7 @@ while (i_p < len(code)):
             r_b = combo % 8
             i_p += 2
         case 3:  # jnz
-            i_p = op_jnz(r_a, literal)
+            i_p = op_jnz(i_p, r_a, literal)
         case 4:  # bxc
             r_b = r_b ^ r_c
             i_p += 2
