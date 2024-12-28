fc8d228ad74063b6d59ed6c282914f4c9a62eddc
Daniel Yordanov <daniyord@gmail.com>
2024-12-28 21:36:37 +0200

    -

--- day22/task2.py
+++ day22/task2.py
@@ -1,7 +1,9 @@
 from shared import calculate_next
 
+best_offers = {}
 
-def calculate_prices(secret, depth):
+
+def calculate_prices(index, secret, depth):
     prev_price = secret % 10
 
     prices = []
@@ -13,16 +15,42 @@ def calculate_prices(secret, depth):
 
         prices.append((diff, price))
 
+        if len(prices) >= 4:
+            price1 = prices[-4]
+            price2 = prices[-3]
+            price3 = prices[-2]
+            price4 = prices[-1]
+
+            # print(price1, price2, price3, price4)
+
+            key = f"{price1[0]}_{price2[0]}_{price3[0]}_{price4[0]}"
+
+            if key not in best_offers:
+                best_offers[key] = {}
+
+            if index not in best_offers[key]:
+                best_offers[key][index] = price4[1]
+
         prev_price = price
 
     return prices
 
 
-print(calculate_prices(123, 2000))
+result = []
+file = open('input.txt', 'r')
+for index, line in enumerate(file):
+    calculate_prices(index, int(line.strip()), 2000)
 
 
-# result = []
-# file = open('input_demo.txt', 'r')
-# for line in file:
-#     print(calculate_prices(int(line.strip()), 10))
-#     exit(0)
+max_price = None
+for key in best_offers:
+
+    price = 0
+    for item in best_offers[key]:
+        price += best_offers[key][item]
+
+    if max_price is None or max_price < price:
+        max_price = price
+
+
+print(max_price)
